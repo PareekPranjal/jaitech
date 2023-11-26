d3cba30dfdb220f6ceaff71443053980d4a0c4fc
Pranjal Pareek <pareekpranjal@outlook.com>
2023-11-26 13:40:25 +0530

    the commit

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <p className="font-playfair  text-deep-blue">
-          Helpline No: +919216939624 <br /> Email Us: contact@jaitechrobotics.com <br /> <span className="font-semibold text-2xl">Jaitech Robotics Pvt.Ltd. All Rights Reserved.</span> 
+          Helpline No: +919216939624 <br /> Email Us: contact@jaitechrobotics.com <br/> <span className="font-semibold text-2xl">Jaitech Robotics Pvt.Ltd. All Rights Reserved.</span> 
           </p>
           <p className="font-playfair text-md text-deep-blue">
           This Website is Developed By Pranjal Pareek <br />pareekpranjal@outlook.com <br /> +9180055574463
